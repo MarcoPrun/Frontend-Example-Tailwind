3583d476c79e53480a71d52798379a5e3ba4c2ae
Marco <marcoprunas.mp@gmail.com>
2021-02-15 18:50:25 +0100

    animations update

--- public/javascript/carousels.js
+++ public/javascript/carousels.js
@@ -2,7 +2,7 @@ var news = new Glide('.news', {
     type: 'carousel',
     autoplay: 4000,
     perView: 3,
-    focusAt: 'center',
+    focusAt: 0,
     gap: 20,
     breakpoints: {
         1300: {
@@ -22,7 +22,7 @@ news.mount();
 var products = new Glide('.products', {
     type: 'carousel',
     perView: 4,
-    focusAt: 1,
+    focusAt: 0,
     gap: 15,
     breakpoints: {
         980: {
