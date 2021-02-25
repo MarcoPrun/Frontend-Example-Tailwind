a457d45271c007c9c9829c5548035f646645b135
Marco <marcoprunas.mp@gmail.com>
2021-02-25 12:53:59 +0100

    added navigation menu

---
+++ public/javascript/script.js
@@ -0,0 +1,22 @@
+// animation changement de couleur nav bar
+
+// const nav = document.getElementById('nav');
+
+// setInterval(function changeBg() {
+//     let navPos = nav.getBoundingClientRect();
+//     if(navPos.top === 0) {
+//         nav.classList.add('mybg');
+//     } else {
+//         nav.classList.remove('mybg');
+//     }
+// }, 100);
+
+// animation menu
+
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    burger.classList.toggle('active');
+});
